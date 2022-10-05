a5e47cde269286f09dc2738342d79ce9c079397c
DanielSanCo <daniel.santos3210@outlook.com>
2022-10-05 12:48:08 -0300

    Atualizando dia 05/10/22

--- Ex04102022/atividade1.sql
+++ Ex04102022/atividade1.sql
@@ -1,67 +1,41 @@
-/*create database db_generation_game_online;*/
-/*use db_generation_game_online;*/
-/*create table tb_personagens (
-	id bigint(5) not null auto_increment,
-    nome varchar(20) not null,
-    idade bigint(4) not null,
-    elemento varchar(20) not null,
-    profissao varchar(20),
-    
-    primary key(id)
-);*/
+/*ATIVIDADE 1*/
+
+/*create database db_generation_game_online;
+use db_generation_game_online;*/
 
 /*create table tb_classes (
 	id bigint(5) not null auto_increment,
     classe varchar(20) not null,
-    arma varchar(20),
+    elemento varchar(20) not null,
     
     primary key (id)
 );*/
+/*create table tb_personagens (
+	id bigint(5) not null auto_increment,
+    nome varchar(30) not null,
+    idade bigint(5) not null,
+    poder bigint(20) not null,
+    defesa bigint(20) not null,
+    id_classe bigint,
+    
+    primary key (id),
+    foreign key (id_classe) references tb_classes(id)
+);*/
 
-/*alter table tb_personagens add foreign key(id_classe) references tb_classes(id);*/
-
-/*insert into tb_classes(classe, arma) values('lutador', 'punho');
-insert into tb_classes(classe, arma) values('Espadachin', 'espada');
-insert into tb_classes(classe, arma) values('Mago', 'cajado');
-insert into tb_classes(classe, arma) values('Bruxo', 'varinha');
-insert into tb_classes(classe, arma) values('Shaman', 'cetro');*/
+/*insert into tb_classes(classe, elemento) values ('suporte', 'luz'),
+('mago', 'trevas'), ('tanque', 'agua'), ('selvagem', 'terra'), ('atirador', 'fogo');*/
 
 /*select * from tb_classes;*/
 
-/*insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Kazin', 80, 'Luz', 'Alquimista', 3);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Dario', 34, 'terra', 'guerreiro', 2);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Morgianna', 19, 'trevas', 'curandeira', 4);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Venancio', 40, 'Fogo', 'Shaman', 5);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Mike', 32, 'Nada', 'Lutador', 1);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Holldor', 50, 'Nada', 'Ex-Guerreiro', 2);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Melgor', 20, 'Luz', 'Curandeiro', 3);
-insert into tb_personagens(nome, idade, elemento, profissao, id_classe) values('Andor', 30, 'Trevas', 'Alquimista', 5);*/
-
-/*alter table tb_personagens add poder bigint(20) not null;*/
-
-/*update tb_personagens set poder = 5000 where id = 1;
-update tb_personagens set poder = 1000 where id = 2;
-update tb_personagens set poder = 1500 where id = 3;
-update tb_personagens set poder = 23000 where id = 4;
-update tb_personagens set poder = 1800 where id = 5;
-update tb_personagens set poder = 899900 where id = 6;
-update tb_personagens set poder = 3800 where id = 7;
-update tb_personagens set poder = 1300 where id = 8;*/
+/*insert into tb_personagens(nome, idade, poder, defesa, id_classe) values
+('Dario Naharis', 34, 1340, 870, 5), ('Stevan Mahul', 97, 7890, 340, 2),
+('Manaceus Kim', 27, 1850, 80, 1), ('Rogue Two', 84, 870, 10230, 3),
+('Kyra Quin', 50, 1450, 1760, 4), ('Warudo', 732, 100, 53400, 3),
+('L.L. Jamau', 38, 780, 1200, 5), ('Aicer Krono', 40, 2001, 20, 2);*/
 
 /*select * from tb_personagens where poder > 2000;*/
-/*select * from tb_personagens where poder > 2000;*/
-
-/*alter table tb_personagens add defesa bigint(20) not null;*/
-
-/*update tb_personagens set defesa = 5000 where id = 1;
-update tb_personagens set defesa = 1000 where id = 2;
-update tb_personagens set defesa = 1500 where id = 3;
-update tb_personagens set defesa = 23000 where id = 4;
-update tb_personagens set defesa = 1800 where id = 5;
-update tb_personagens set defesa = 899900 where id = 6;
-update tb_personagens set defesa = 3800 where id = 7;
-update tb_personagens set defesa = 1300 where id = 8;*/
-
 /*select * from tb_personagens where defesa > 1000 and defesa < 2000;*/
-
 /*select * from tb_personagens where nome like '%c%';*/
+
+/*select * from tb_personagens inner join tb_classes on tb_classes.id = tb_personagens.id_classe;*/
+/*select * from tb_personagens inner join tb_classes on tb_classes.id = tb_personagens.id_classe where classe = 'tanque';*/
